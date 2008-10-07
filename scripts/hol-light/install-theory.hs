873442e1cd7ecf91b4b38ac886e845b7b642bfae
gilith <gilith>
2008-10-07 23:51:56 +0000

    preprocessing theory files for the hol-light test

---
+++ scripts/hol-light/install-theory.hs
@@ -0,0 +1,47 @@
+#!/usr/bin/runhaskell
+
+import qualified Control.Monad
+import qualified Data.List as List
+import qualified System.IO
+import qualified Text.Regex
+
+data State =
+    RestState
+  | ProveState
+  deriving (Eq, Show, Ord)
+
+getLines :: System.IO.Handle -> IO [String]
+getLines h =
+    do s <- System.IO.hGetContents h
+       return (lines s)
+
+proveRegex :: Text.Regex.Regex
+proveRegex = Text.Regex.mkRegex "^let ([[:alnum:]_]+) *= *prove *$"
+
+endRegex :: Text.Regex.Regex
+endRegex = Text.Regex.mkRegex ";; *$"
+
+process :: State -> String -> (State,String)
+process s l =
+    case s of
+      RestState ->
+          case Text.Regex.matchRegexAll proveRegex l of
+            Just (_,_,_,[l1]) ->
+                (ProveState, "let " ++ l1 ++ " = lemma (fun () -> prove")
+            Just _ -> error "bad match"
+            Nothing -> (s,l)
+      ProveState ->
+          case Text.Regex.matchRegexAll endRegex l of
+            Just (l1,_,_,[]) ->
+                (RestState, l1 ++ ");;")
+            Just _ -> error "bad match"
+            Nothing -> (s,l)
+
+main :: IO ()
+main =
+    do ls <- getLines System.IO.stdin
+       let (s,ls') = List.mapAccumL process RestState ls
+       case s of
+         RestState -> return ()
+         ProveState -> error "eof inside a prove"
+       Control.Monad.forM_ ls' putStrLn
