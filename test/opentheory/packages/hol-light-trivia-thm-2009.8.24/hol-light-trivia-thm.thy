34a25f6425b21341d6d619b4f6cee238a8d67a66
Joe Hurd <joe@galois.com>
2010-01-09 20:17:56 -0800

    Propagating name change through trivia

---
+++ test/opentheory/packages/hol-light-trivia-thm-2009.8.24/hol-light-trivia-thm.thy
@@ -0,0 +1,19 @@
+name: hol-light-trivia-thm
+version: 2009.8.24
+description: HOL Light trivia theory theorems
+author: Joe Hurd <joe@gilith.com>
+license: HOLLight
+
+require comb-thm {
+  package: hol-light-trivia-comb-thm-2009.8.24
+}
+
+require one-thm {
+  import: comb-thm
+  package: hol-light-trivia-one-thm-2009.8.24
+}
+
+theory {
+  import comb-thm;
+  import one-thm;
+}
