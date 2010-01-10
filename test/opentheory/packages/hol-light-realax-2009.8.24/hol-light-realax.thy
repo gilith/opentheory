23d5c9fb2a4d8943e817575fbc5c67656818c6fb
Joe Hurd <joe@galois.com>
2010-01-10 14:08:15 -0800

    Testing the name change

--- test/opentheory/packages/hol-light-realax-2009.8.24/hol-light-realax.thy
+++ test/opentheory/packages/hol-light-realax-2009.8.24/hol-light-realax.thy
@@ -4,4 +4,10 @@ description: HOL Light realax theory
 author: Joe Hurd <joe@gilith.com>
 license: HOLLight
 
-theory { article "realax.art"; }
+require thm {
+  package: hol-light-realax-thm-2009.8.24
+}
+
+theory {
+  import thm;
+}
