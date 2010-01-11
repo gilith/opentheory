b14942f0afd896428bae3ec5d16235e19ef2d81d
Joe Hurd <joe@galois.com>
2010-01-10 19:08:47 -0800

    Name change porting

--- test/opentheory/packages/cond-def-1.0/cond-def.thy
+++ test/opentheory/packages/cond-def-1.0/cond-def.thy
@@ -4,78 +4,78 @@ description: Definition of the conditional
 author: Joe Hurd <joe@gilith.com>
 license: PublicDomain
 
-require bool-rule {
-  package: hol-light-bool-rule-2009.8.24
+require bool-thm {
+  package: hol-light-bool-thm-2009.8.24
 }
 
-require tactics {
-  import: bool-rule
-  package: hol-light-tactics-2009.8.24
+require tactics-thm {
+  import: bool-thm
+  package: hol-light-tactics-thm-2009.8.24
 }
 
-require simp {
-  import: bool-rule
-  import: tactics
-  package: hol-light-simp-2009.8.24
+require simp-thm {
+  import: bool-thm
+  import: tactics-thm
+  package: hol-light-simp-thm-2009.8.24
 }
 
-require theorems {
-  import: bool-rule
-  import: tactics
-  import: simp
-  package: hol-light-theorems-2009.8.24
+require theorems-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  package: hol-light-theorems-thm-2009.8.24
 }
 
-require ind-defs {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  package: hol-light-ind-defs-2009.8.24
+require ind-defs-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  package: hol-light-ind-defs-thm-2009.8.24
 }
 
 require class-eta-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
   package: hol-light-class-eta-thm-2009.8.24
 }
 
 require class-select-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
   import: class-eta-thm
   package: hol-light-class-select-thm-2009.8.24
 }
 
-require def {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
+require class-cond-def {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
   import: class-eta-thm
   import: class-select-thm
   package: hol-light-class-cond-def-2009.8.24
 }
 
-require alt {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
+require class-cond-alt {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
   import: class-eta-thm
   import: class-select-thm
-  import: def
+  import: class-cond-def
   package: hol-light-class-cond-alt-2009.8.24
 }
 
 theory {
-  import alt;
+  import class-cond-alt;
 }
--- test/opentheory/packages/numeral-def-1.0/numeral-def.thy
+++ test/opentheory/packages/numeral-def-1.0/numeral-def.thy
@@ -1,257 +1,157 @@
-name: num-def
+name: numeral-def
 version: 1.0
-description: Definition of the natural numbers
+description: Definition of bitwise natural number numerals
 author: Joe Hurd <joe@gilith.com>
 license: PublicDomain
 
-require bool-rule {
-  package: hol-light-bool-rule-2009.8.24
+require bool-thm {
+  package: hol-light-bool-thm-2009.8.24
 }
 
-require tactics {
-  import: bool-rule
-  package: hol-light-tactics-2009.8.24
+require tactics-thm {
+  import: bool-thm
+  package: hol-light-tactics-thm-2009.8.24
 }
 
-require simp {
-  import: bool-rule
-  import: tactics
-  package: hol-light-simp-2009.8.24
+require simp-thm {
+  import: bool-thm
+  import: tactics-thm
+  package: hol-light-simp-thm-2009.8.24
 }
 
-require theorems {
-  import: bool-rule
-  import: tactics
-  import: simp
-  package: hol-light-theorems-2009.8.24
+require theorems-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  package: hol-light-theorems-thm-2009.8.24
 }
 
-require ind-defs {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  package: hol-light-ind-defs-2009.8.24
+require ind-defs-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  package: hol-light-ind-defs-thm-2009.8.24
 }
 
-require class-eta-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  package: hol-light-class-eta-thm-2009.8.24
+require class-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  package: hol-light-class-thm-2009.8.24
 }
 
-require class-select-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  package: hol-light-class-select-thm-2009.8.24
+require trivia-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  package: hol-light-trivia-thm-2009.8.24
 }
 
-require class-cond-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  package: hol-light-class-cond-thm-2009.8.24
+require canon-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  package: hol-light-canon-thm-2009.8.24
 }
 
-require class-skolem {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  package: hol-light-class-skolem-2009.8.24
+require meson-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  package: hol-light-meson-thm-2009.8.24
 }
 
-require class-bool {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  package: hol-light-class-bool-2009.8.24
-}
-
-require trivia-comb {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  package: hol-light-trivia-comb-2009.8.24
-}
-
-require trivia-one-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  package: hol-light-trivia-one-thm-2009.8.24
-}
-
-require canon {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  package: hol-light-canon-2009.8.24
-}
-
-require meson {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  package: hol-light-meson-2009.8.24
-}
-
-require quot {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  import: meson
-  package: hol-light-quot-2009.8.24
+require quot-thm {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  import: meson-thm
+  package: hol-light-quot-thm-2009.8.24
 }
 
 require pair-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  import: meson
-  import: quot
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  import: meson-thm
+  import: quot-thm
   package: hol-light-pair-thm-2009.8.24
 }
 
-require numeral {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  import: meson
-  import: quot
+require num-numeral {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  import: meson-thm
+  import: quot-thm
   import: pair-thm
   package: hol-light-num-numeral-2009.8.24
 }
 
 require num-thm {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  import: meson
-  import: quot
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  import: meson-thm
+  import: quot-thm
   import: pair-thm
-  import: numeral
+  import: num-numeral
   package: hol-light-num-thm-2009.8.24
 }
 
-require bit {
-  import: bool-rule
-  import: tactics
-  import: simp
-  import: theorems
-  import: ind-defs
-  import: class-eta-thm
-  import: class-select-thm
-  import: class-cond-thm
-  import: class-skolem
-  import: class-bool
-  import: trivia-comb
-  import: trivia-one-thm
-  import: canon
-  import: meson
-  import: quot
+require num-bit {
+  import: bool-thm
+  import: tactics-thm
+  import: simp-thm
+  import: theorems-thm
+  import: ind-defs-thm
+  import: class-thm
+  import: trivia-thm
+  import: canon-thm
+  import: meson-thm
+  import: quot-thm
   import: pair-thm
-  import: numeral
+  import: num-numeral
   import: num-thm
   package: hol-light-num-bit-2009.8.24
 }
 
 theory {
-  import numeral;
-  import bit;
+  import num-numeral;
+  import num-bit;
 }
