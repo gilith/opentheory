f096d78ad85df90760cf1f32a46682c37fa32b58
Joe Hurd <joe@galois.com>
2010-07-05 11:54:51 -0700

    Bool theory.

--- test/theories/bool-not-def/bool-not-def.thy
+++ test/theories/bool-not-def/bool-not-def.thy
@@ -3,6 +3,7 @@ version: 1.0
 description: HOL Light theory segment bool-not-def extracted on 2010-07-01.
 author: Joe Hurd <joe@gilith.com>
 license: HOLLight
+show: "Data.Bool"
 
 main {
   article: "bool-not-def.art"
