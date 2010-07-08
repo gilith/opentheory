31f590304ba46c5ffa3890ac88d47be6bec8a6da
Joe Hurd <joe@galois.com>
2010-07-08 12:21:47 -0700

    Added some test packages.

---
+++ test/directory/packages/bool-true-thm-1.0/bool-true-thm.thy
@@ -0,0 +1,10 @@
+name: bool-true-thm
+version: 1.0
+description: HOL Light theory segment bool-true-thm extracted on 2010-07-01.
+author: Joe Hurd <joe@gilith.com>
+license: HOLLight
+show: "Data.Bool"
+
+main {
+  article: "bool-true-thm.art"
+}
