f4158353d514ea8669639bbd8f3029749c1627ae
Joe Hurd <joe@gilith.com>
2010-12-05 14:40:30 -0800

    Breaking the pre properties into agreed and controversial

--- data/theories/natural-pre/natural-pre.thy
+++ data/theories/natural-pre/natural-pre.thy
@@ -1,6 +1,6 @@
-name: natural-fact
+name: natural-pre
 version: 1.0
-description: Definitions and theorems about natural number factorial
+description: Definitions and theorems about the natural number pre function
 author: Joe Hurd <joe@gilith.com>
 license: OpenTheory
 show: "Data.Bool"
@@ -8,12 +8,12 @@ show: "Number.Natural"
 show: "Number.Numeral"
 
 def {
-  package: natural-fact-def-1.0
+  package: natural-pre-def-1.0
 }
 
 thm {
   import: def
-  package: natural-fact-thm-1.0
+  package: natural-pre-thm-1.0
 }
 
 main {
