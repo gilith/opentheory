0dc68563cf4aba8e4ee1914dd4e2a6ed12b814dc
Joe Hurd <joe@gilith.com>
2011-01-07 11:44:48 -0800

    Lazy parsing

--- data/haskell/example-utf8/src/OpenTheory/Parser.hs
+++ data/haskell/example-utf8/src/OpenTheory/Parser.hs
@@ -7,7 +7,36 @@ License-file: LICENSE
 Maintainer: Joe Hurd
 -}
 module OpenTheory.Parser
-  ( Parser(..) )
+  ( Token(..),
+    fromList,
+    toList,
+    Parser(..),
+    parse )
 where
 
-newtype Parser a b = Parser { unParser :: [a] -> Maybe (b,[a]) }
+data Token a =
+    Error
+  | Eof
+  | Token a (Token a)
+
+fromList :: [a] -> Token a
+fromList [] = Eof
+fromList (a : t) = Token a (fromList t)
+
+toList :: Token a -> Maybe [a]
+toList Error = Nothing
+toList Eof = Just []
+toList (Token a t) =
+    case toList t of
+      Just t' -> Just (a : t')
+      Nothing -> Nothing
+
+newtype Parser a b = Parser { unParser :: (a, Token a) -> Maybe (b, Token a) }
+
+parse :: Parser a b -> Token a -> Token b
+parse _ Error = Error
+parse _ Eof = Eof
+parse p (Token a t) =
+    case unParser p (a,t) of
+      Nothing -> Error
+      Just (b,t') -> Token b (parse p t')
