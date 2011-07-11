2e43ebbe0106d5115496f81dc1e83592706392e7
Joe Hurd <joe@gilith.com>
2011-07-10 17:37:06 -0700

    Adding real numbers to base

---
+++ data/theories/real-thm/real-thm.thy
@@ -0,0 +1,11 @@
+name: real-thm
+version: 1.0
+description: real-thm
+author: Joe Hurd <joe@gilith.com>
+license: HOLLight
+provenance: HOL Light theory extracted on 2011-07-10
+show: "Data.Bool"
+
+main {
+  article: "real-thm.art"
+}
