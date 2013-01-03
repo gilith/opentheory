ea0c6e0e57613a3a8ada1217d4323033dafe65bf
Joe Leslie-Hurd <joe@gilith.com>
2013-01-02 20:49:03 -0800

    Fixed twitter stream

--- repo/index.php
+++ repo/index.php
@@ -607,7 +607,7 @@ pretty_recent_packages(SHORT_RECENT_PACKAGE_LIMIT);
 $image =
   array(site_image('silver-falls.jpg','Silver Falls'),
         ('<div id="twitter-wrapper"><div id="twitter"><p><a href="http://twitter.com/OpenTheory">OpenTheory twitter feed:</a></p><ul id="twitter_update_list"><li>Loading...</li></ul></div></div>' .
-         '<script src="http://twitter.com/javascripts/blogger.js" type="text/javascript"></script><script src="http://twitter.com/statuses/user_timeline/OpenTheory.json?callback=twitterCallback2&amp;count=4" type="text/javascript"></script>'));
+         '<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script><script type="text/javascript" src="http://api.twitter.com/1/statuses/user_timeline.json?screen_name=OpenTheory&include_rts=true&count=3&callback=twitterCallback2"></script>'));
 
 output(array(), $main, $image);
 
