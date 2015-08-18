9d60dba3089a22a4f43d6694cd7026b6edc941af
Joe Leslie-Hurd <joe@gilith.com>
2015-08-18 03:23:36 +0000

    More continued fractions

--- data/haskell/arithmetic/src/Arithmetic/ContinuedFraction.hs
+++ data/haskell/arithmetic/src/Arithmetic/ContinuedFraction.hs
@@ -13,8 +13,39 @@ where
 import OpenTheory.Primitive.Natural
 
 newtype ContinuedFraction =
-    ContinuedFraction {unContinuedFraction :: [Natural]}
+    ContinuedFraction {unContinuedFraction :: (Natural,[Natural])}
+  deriving Eq
 
+fromNatural :: Natural -> ContinuedFraction
+fromNatural n = ContinuedFraction (n,[])
+
+goldenRatio :: ContinuedFraction
+goldenRatio = ContinuedFraction (1, repeat 1)
+
+approximate :: (Natural -> a) -> (a -> a -> a) -> (a -> a) ->
+               ContinuedFraction -> Natural -> a
+approximate lift add inv =
+    \ (ContinuedFraction (n,l)) -> go (lift n) l
+  where
+    go n l depth =
+        if depth == 0 then n
+        else case l of
+               [] -> n
+               h : t -> add n (inv (go (lift h) t (depth - 1)))
+
+toFractional :: Fractional a => ContinuedFraction -> Natural -> a
+toFractional =
+    approximate fromIntegral (+) inv
+  where
+    inv x = 1.0 / x
+
+toDouble :: ContinuedFraction -> Double
+toDouble x = toFractional x 53
+
+instance Show ContinuedFraction where
+  show = show . toDouble
+
+{-
 fromRealFrac :: RealFrac a => a -> ContinuedFraction
 fromRealFrac =
     ContinuedFraction . go
@@ -23,3 +54,4 @@ fromRealFrac =
         n : (if y == 0.0 then [] else go (1.0 / y))
       where
         (n,y) = properFraction x
+-}
