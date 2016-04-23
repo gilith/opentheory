e401f306167b0c4539ed0a354f8fe53e1c4b87b6
Joe Leslie-Hurd <joe@gilith.com>
2016-04-23 20:01:39 +0000

    Factor out the interpretation

--- data/theories/byte-def/byte-def.thy
+++ data/theories/byte-def/byte-def.thy
@@ -1,5 +1,5 @@
 name: byte-def
-version: 1.97
+version: 1.98
 description: Definition of bytes
 author: Joe Leslie-Hurd <joe@gilith.com>
 license: MIT
@@ -21,29 +21,7 @@ def {
 
 word {
   import: def
-  interpret: type "Data.Word.word" as "Data.Byte.byte"
-  interpret: const "Data.Word.*" as "Data.Byte.*"
-  interpret: const "Data.Word.+" as "Data.Byte.+"
-  interpret: const "Data.Word.-" as "Data.Byte.-"
-  interpret: const "Data.Word.<" as "Data.Byte.<"
-  interpret: const "Data.Word.<=" as "Data.Byte.<="
-  interpret: const "Data.Word.^" as "Data.Byte.^"
-  interpret: const "Data.Word.~" as "Data.Byte.~"
-  interpret: const "Data.Word.and" as "Data.Byte.and"
-  interpret: const "Data.Word.bit" as "Data.Byte.bit"
-  interpret: const "Data.Word.fromNatural" as "Data.Byte.fromNatural"
-  interpret: const "Data.Word.modulus" as "Data.Byte.modulus"
-  interpret: const "Data.Word.not" as "Data.Byte.not"
-  interpret: const "Data.Word.or" as "Data.Byte.or"
-  interpret: const "Data.Word.random" as "Data.Byte.random"
-  interpret: const "Data.Word.shiftLeft" as "Data.Byte.shiftLeft"
-  interpret: const "Data.Word.shiftRight" as "Data.Byte.shiftRight"
-  interpret: const "Data.Word.toNatural" as "Data.Byte.toNatural"
-  interpret: const "Data.Word.width" as "Data.Byte.width"
-  interpret: const "Data.Word.Bits.compare" as "Data.Byte.Bits.compare"
-  interpret: const "Data.Word.Bits.fromWord" as "Data.Byte.Bits.fromByte"
-  interpret: const "Data.Word.Bits.normal" as "Data.Byte.Bits.normal"
-  interpret: const "Data.Word.Bits.toWord" as "Data.Byte.Bits.toByte"
+  interpretation: "word.int"
   package: word-1.120
 }
 
